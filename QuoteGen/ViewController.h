1c42051b4f18b8c5a6ed93cf0d92175bc10953c3
ROHITH SALIM <rohithsalim@ROHITHs-MacBook-Pro.local>
2013-07-09 12:08:42 -0400

    Initial Commit

---
+++ QuoteGen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuoteGen
+//
+//  Created by ROHITH SALIM on 7/9/13.
+//  Copyright (c) 2013 ROHITH SALIM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QuoteGen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuoteGen
+//
+//  Created by ROHITH SALIM on 7/9/13.
+//  Copyright (c) 2013 ROHITH SALIM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
